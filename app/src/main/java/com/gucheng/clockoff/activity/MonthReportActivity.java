3f2bf23cf3221c4c480e28bba103faf3fdd9127f
gucheng <459843397@qq.com>
2018-08-14 22:43:55 +0800

    查询月报表，新建adapter

--- app/src/main/java/com/gucheng/clockoff/activity/MonthReportActivity.java
+++ app/src/main/java/com/gucheng/clockoff/activity/MonthReportActivity.java
@@ -1,6 +1,7 @@
 package com.gucheng.clockoff.activity;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +10,8 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 
+import com.gucheng.clockoff.DBHelper;
+import com.gucheng.clockoff.DBManger;
 import com.gucheng.clockoff.R;
 import com.gucheng.clockoff.db.MonthTable;
 
@@ -21,12 +24,23 @@ import java.util.ArrayList;
 public class MonthReportActivity extends AppCompatActivity {
     private ListView mListView;
     private ArrayList<MonthTable.MonthReportItem> mDatas = null;
+    private DBManger mDbManager = DBManger.getInstance(this);
+    private DBHelper mDbHelper;
+    private SQLiteDatabase mDatabase;
+    private MyAdapter mMonthAdapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_month_report);
         mListView = (ListView) findViewById(R.id.month_list);
+        mDbHelper = new DBHelper(this);
+        mDatabase = mDbHelper.getWritableDatabase();
+        mDatas = MonthTable.queryAll(mDatabase);
+        mMonthAdapter = new MyAdapter(this, mDatas);
+        mListView.setAdapter(mMonthAdapter);
+        mDatabase.close();
+
     }
 
     class MyAdapter extends BaseAdapter {
